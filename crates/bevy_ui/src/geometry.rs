c7dc55d483db4dbf99110ccdc83c34aae253237a
66OJ66 <59021291+66OJ66@users.noreply.github.com>
2026-05-14 18:05:01 +0100

    Update geometry.rs

--- crates/bevy_ui/src/geometry.rs
+++ crates/bevy_ui/src/geometry.rs
@@ -562,7 +562,7 @@ pub fn vmax<T: ValNum>(value: T) -> Val {
 #[cfg_attr(
     feature = "serialize",
     derive(serde::Serialize, serde::Deserialize),
-    serde(default)
+    serde(default),
     reflect(Serialize, Deserialize)
 )]
 pub struct UiRect {
